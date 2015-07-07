351d3d99c441cd454002c64d4519bdebbbbe973e
Ayumu AIZAWA <ayumu.aizawa@gmail.com>
2015-07-07 20:33:22 +0900

    fix default url option for productions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net' }
-  
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_DEFAULT_URL_OPTION'] || '' }
+
 end
